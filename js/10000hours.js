cc0c03326a29cf7f0caab9308d387f22e85574ba
tnghgks <tnghgks@naver.com>
2022-09-15 20:31:16 +0900

    js complete

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,12 +1,41 @@
 const modal = document.querySelector(".cont-modal");
 const popUpBtn = document.querySelector("#practice_btn");
 const popUpCloseBtn = document.querySelector("#popup-close");
+const form = document.querySelector("#form-wannabe");
+const resultWrapper = document.querySelector("#result-wrapper");
+const resultSection = document.querySelector("#result");
+const loadingImg = document.querySelector("#loading_img");
 
 let display = "none";
 
+function popUpVisible() {
+  modal.classList.toggle("modal");
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  resultSection.classList.remove("show");
+  loadingImg.classList.add("show");
+  setTimeout(timeDivison, 1800);
+}
+
+function timeDivison() {
+  loadingImg.classList.remove("show");
+  const wannabe = document.querySelector("#wannabe");
+  const time = document.querySelector("#time");
+  const resultWannabe = document.querySelector("#result-wannabe");
+  const resultTime = document.querySelector("#result-time");
+
+  if (isNaN(time.value / 1)) {
+    alert("시간에는 숫자만 입력해주세요.");
+    return;
+  }
+
+  resultWannabe.innerHTML = wannabe.value;
+  resultTime.innerHTML = Math.floor(10000 / parseInt(time.value, 10));
+  resultSection.classList.add("show");
+}
+
 popUpBtn.addEventListener("click", popUpVisible);
 popUpCloseBtn.addEventListener("click", popUpVisible);
-
-function popUpVisible() {
-  modal.classList.toggle("show");
-}
+form.addEventListener("submit", handleSubmit);
